371bc52e88495b481efa6d86c99eefe8396ed6e9
Juan Antonio <1317580+JuantonCodex@users.noreply.github.com>
2024-05-27 00:13:54 -0500

    feat: add open url feature with kmp multiplatform library

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,6 +1,7 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -8,21 +9,43 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.multiplatform.webview.util.KLogSeverity
+import com.multiplatform.webview.web.WebView
+import com.multiplatform.webview.web.rememberWebViewNavigator
+import com.multiplatform.webview.web.rememberWebViewState
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import webviewapp.composeapp.generated.resources.Res
-import webviewapp.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
-    Column (
+    val url = "https://www.google.com"
+    val state = rememberWebViewState(url)
+    var showWebView by remember { mutableStateOf(false) }
+    LaunchedEffect(Unit) {
+        state.webSettings.apply {
+            logSeverity = KLogSeverity.Debug
+            isJavaScriptEnabled = true
+            customUserAgentString =
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/625.20 (KHTML, like Gecko) Version/14.3.43 Safari/625.20"
+        }
+    }
+
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
-    ){
-        Button(onClick = {}) {
+    ) {
+        Button(onClick = {
+            showWebView = true
+        }) {
             Text("Open WebView")
+        }
+        AnimatedVisibility(visible = showWebView) {
+            WebView(
+                state = state,
+                modifier = Modifier.fillMaxSize(),
+            )
         }
     }
 }
